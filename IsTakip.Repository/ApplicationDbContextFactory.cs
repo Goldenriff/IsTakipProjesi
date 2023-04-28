e09fb07cd93dfe6533a060c852132e1e33919452
Goldenriff <kutluatautku@gmail.com>
2023-04-28 13:51:59 +0300

    Is Takip Project
    Siliconmade Görev 15

---
+++ IsTakip.Repository/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace IsTakip.Repository
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Gorev11; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
